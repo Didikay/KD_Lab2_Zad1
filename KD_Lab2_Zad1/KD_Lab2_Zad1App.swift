9b9810ed93e1ba44add02269060dfb2817db5170
user <user@account.com>
2024-04-27 11:04:17 +0200

    Initial Commit

---
+++ KD_Lab2_Zad1/KD_Lab2_Zad1App.swift
@@ -0,0 +1,17 @@
+//
+//  KD_Lab2_Zad1App.swift
+//  KD_Lab2_Zad1
+//
+//  Created by student on 27/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct KD_Lab2_Zad1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
